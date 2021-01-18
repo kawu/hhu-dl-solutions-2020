Zero-out gradients after optim.step() and fix #1

4/ex3.py
@@ -26,7 +26,7 @@ model = create_model(
 )
 
 # TODO: Create optimizer (e.g. torch.optim.Adam) with appropriate arguments
-optim = torch.optim.Adam(model.parameters())
+optim = torch.optim.Adam(model.parameters(), lr=1.0)
 
 # Perform SGD for a selected number of epochs
 epoch_num = 50    # TODO: change to None
@@ -35,7 +35,8 @@ for k in range(epoch_num):
         # TODO: Calculate the loss, call backward
         # TODO: Apply the optimisation step
         calculate_loss(model(x), y).backward()
-        optim.step()	# version of `nudge` provided by `Adam`
+        optim.step()        # version of `nudge` provided by `Adam`
+        optim.zero_grad()   # reset to zero all the gradients
 
 #################################################
 # EVALUATION SECTION: DO NOT MODIFY

7/ex2_v1.py
@@ -58,9 +58,14 @@ model = nn.Sequential(
 #############################################
 
 train(model, loss, accuracy, enc_train, enc_dev, epoch_num=3,
-        learning_rate=0.00001, report_rate=1)
+        learning_rate=0.001, report_rate=1)
 train(model, loss, accuracy, enc_train, enc_dev, epoch_num=2,
-        learning_rate=0.000001, report_rate=1)
+        learning_rate=0.0001, report_rate=1)
+# => @1: loss(train)=14411.733, acc(train)=0.822, acc(dev)=0.808
+# => @2: loss(train)=9076.391, acc(train)=0.871, acc(dev)=0.824
+# => @3: loss(train)=6773.723, acc(train)=0.911, acc(dev)=0.829
+# => @1: loss(train)=3628.002, acc(train)=0.952, acc(dev)=0.845
+# => @2: loss(train)=2555.633, acc(train)=0.963, acc(dev)=0.845
 
 #############################################
 # EVALUATION